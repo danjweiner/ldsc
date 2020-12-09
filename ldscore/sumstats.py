9c447bcbd853fabc9ca3a76de57db86f41cbffbb
danjweiner <danweiner92@gmail.com>
2020-12-09 17:36:57 -0500

    corrected n_snp output

--- ldscore/sumstats.py
+++ ldscore/sumstats.py
@@ -377,9 +377,8 @@ def estimate_h2(args, log):
     #pd.DataFrame(chisq).to_csv(args.out+'.chisq', sep="\t", index=False, header = False)
     #pd.DataFrame(ref_ld).to_csv(args.out+'.design', sep="\t", index=False, header = False)
     #pd.DataFrame(s(sumstats[w_ld_cname])).to_csv(args.out+'.weights', sep="\t", index=False, header = False)
-    pd.DataFrame(M_annot).to_csv(args.out+'.mannot', sep="\t", index=False, header = False)
-    pd.DataFrame(s(sumstats.N)).to_csv(args.out+'.ssn', sep="\t", index=False, header = False)  
-    
+    pd.DataFrame(n_snp).to_csv(args.out+'.nsnp', sep="\t", index=False, header = False)
+   
     return hsqhat
 
 
