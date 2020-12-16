bbc684cb6eb103c9e18cccf8aa626d16424f0897
danjweiner <danweiner92@gmail.com>
2020-12-16 17:20:39 -0500

    added back design w compression

--- ldscore/sumstats.py
+++ ldscore/sumstats.py
@@ -374,8 +374,8 @@ def estimate_h2(args, log):
         log.log('Results printed to '+args.out+'.results')
     
     pd.DataFrame(np.linalg.lstsq(ref_ld, chisq)[1]).to_csv(args.out+'.sse', sep="\t", index=False, header = False)
-    #pd.DataFrame(chisq).to_csv(args.out+'.chisq', sep="\t", index=False, header = False)
-    #pd.DataFrame(ref_ld).to_csv(args.out+'.design', sep="\t", index=False, header = False)
+    pd.DataFrame(chisq).to_csv(args.out+'.chisq', sep="\t", index=False, header = False)
+    pd.DataFrame(ref_ld).to_csv(args.out+'.design', sep="\t", index=False, header = False, compression = 'gzip')
     #pd.DataFrame(s(sumstats[w_ld_cname])).to_csv(args.out+'.weights', sep="\t", index=False, header = False)
     pd.DataFrame([n_snp]).to_csv(args.out+'.nsnp', sep="\t", index=False, header = False)
    
